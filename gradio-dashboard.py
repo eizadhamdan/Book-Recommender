2d02a978a2399bd8b5845ab86d266da00295979f
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2025-08-16 12:29:13 +0200

    create dashboard

---
+++ gradio-dashboard.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+import gradio as gr
+
+
+load_dotenv()
+
+books = pd.read_csv("C:\\Codes\\GenAI\\Book-Recommender\\data\\books_with_emotions.csv")
+
+books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"
+books["large_thumbnail"] = np.where(
+    books["large_thumbnail"].isna(),
+    "C:\\Codes\\GenAI\\Book-Recommender\\data\\cover-not-found.jpg",
+    books["large_thumbnail"],
+)
+
+raw_documents = TextLoader("C:\\Codes\\GenAI\\Book-Recommender\\data\\tagged_description.txt").load()
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=0, chunk_overlap=0)
+documents = text_splitter.split_documents(raw_documents)
+db_books = Chroma.from_documents(
+    documents,
+    OpenAIEmbeddings()
+)
+
+def retrieve_semantic_recommendations(
+        query: str,
+        category: str = None,
+        tone: str = None,
+        initial_top_k: int = 50,
+        final_top_k: int = 16,
+) -> pd.DataFrame:
+    
+    recs = db_books.similarity_search(query, k=initial_top_k)
+    books_list = [int(rec.page_content.split('"')[0]) for rec in recs]
+    book_recs = books[books["isbn13"].isin(books_list)].head(final_top_k)
+
+    if category != "All":
+        book_recs = book_recs[book_recs["simple_categories"] == category][:final_top_k]
+    else:
+        book_recs = book_recs.head(final_top_k)
+    
+    if tone == "Happy":
+        book_recs.sort_values(by="joy", ascending=False, inplace=True)
+    elif tone == "Surprising":
+        book_recs.sort_values(by="surprise", ascending=False, inplace=True)
+    elif tone == "Angry":
+        book_recs.sort_values(by="anger", ascending=False, inplace=True)
+    elif tone == "Suspenseful":
+        book_recs.sort_values(by="fear", ascending=False, inplace=True)
+    elif tone == "Sad":
+        book_recs.sort_values(by="sadness", ascending=False, inplace=True)
+
+    return book_recs
